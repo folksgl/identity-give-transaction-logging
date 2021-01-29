c1e71c517d5a81d17a12953a7240b058558e690b
DMITRIY.ZASLAVSKIY <dzaslavskiy@easydynamics.com>
2021-01-29 14:00:40 -0500

    fixup name refactor

--- transaction_log/urls.py
+++ transaction_log/urls.py
@@ -1,4 +1,4 @@
-"""transaction_logging URL Configuration
+"""transaction_log URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- transaction_log/wsgi.py
+++ transaction_log/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for transaction_logging project.
+WSGI config for transaction_log project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
