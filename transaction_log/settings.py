47f468b8c4c902de0887d3c06cf80da9c6578673
DMITRIY.ZASLAVSKIY <dzaslavskiy@easydynamics.com>
2021-01-29 16:24:53 -0500

    Rename Api django app

--- transaction_log/settings.py
+++ transaction_log/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "transaction_logging_api.apps.TransactionLoggingApiConfig",
+    "api.apps.ApiConfig",
     "rest_framework",
 ]
 
