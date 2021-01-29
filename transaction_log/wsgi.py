1f9e35ce5fb46c979104830908a881258ce9adbf
DMITRIY.ZASLAVSKIY <dzaslavskiy@easydynamics.com>
2021-01-29 13:07:03 -0500

    refactor django project
    move package folder to top level
    rename to transaction_log

--- transaction_log/wsgi.py
+++ transaction_log/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transaction_logging.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transaction_log.settings")
 
 application = get_wsgi_application()
